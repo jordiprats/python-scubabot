b02142b537a6d7628e1da245889ddbd7af0159d7
jordiprats <jordi.prats@gmail.com>
2019-04-05 19:46:25 +0200

    bugfix

--- pymeteoapi/__init__.py
+++ pymeteoapi/__init__.py
@@ -1,7 +1,8 @@
 import urllib.request, json
 
 def llista_platjes(latitude, longitude):
-    print("http://meteoapi.systemadmin.es/platges/geosearch/"+str(longitude)+"/"+str(latitude))
-    with urllib.request.urlopen("http://meteoapi.systemadmin.es/platges/geosearch/"+str(longitude)+"/"+str(latitude)) as url:
+    query_url = "http://meteoapi.systemadmin.es/platges/geosearch/"+str(latitude)+"/"+str(longitude)
+    print(query_url)
+    with urllib.request.urlopen(query_url) as url:
         data = json.loads(url.read().decode())
         return str(data)
