da7aaefb5e8ec8a8a68c552ccda75f11f8aaf2df
jordiprats <jordi.prats@gmail.com>
2019-04-06 17:03:02 +0200

    configurable limit

--- pymeteoapi/__init__.py
+++ pymeteoapi/__init__.py
@@ -1,7 +1,7 @@
 import urllib.request, json
 
-def llista_platjes(latitude, longitude):
-    query_url = "http://meteoapi.systemadmin.es/platges/geosearch/"+str(latitude)+"/"+str(longitude)+"/4"
+def llista_platjes(latitude, longitude, limit=4):
+    query_url = "http://meteoapi.systemadmin.es/platges/geosearch/"+str(latitude)+"/"+str(longitude)+"/"+str(limit)
     print(query_url)
     with urllib.request.urlopen(query_url) as url:
         data = json.loads(url.read().decode())
