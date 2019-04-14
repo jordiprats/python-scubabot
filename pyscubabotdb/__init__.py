156726350af5a18dfae734b797533f95b5caac44
jordiprats <jordi.prats@gmail.com>
2019-04-14 21:32:18 +0200

    funcio db query

--- pyscubabotdb/__init__.py
+++ pyscubabotdb/__init__.py
@@ -4,26 +4,24 @@ import MySQLdb
 scubabotdb_user=''
 scubabotdb_password=''
 scubabotdb_db=''
+scubabotdb_host='localhost'
 
-def setup(user,password,db):
-    global scubabotdb_user,scubabotdb_password, scubabotdb_db
+def setup(user,password,db,host='localhost'):
+    global scubabotdb_user,scubabotdb_password, scubabotdb_db, scubabotdb_host
     scubabotdb_user=user
     scubabotdb_password=password
     scubabotdb_db=db
+    scubabotdb_host=host
 
-def setUserPlatja(user_id,platja_id):
+def runDBquery(db_query):
+    global scubabotdb_user,scubabotdb_password, scubabotdb_db, scubabotdb_host
     try:
-        db_query = """
-                INSERT INTO bussejadors (telegram_id, platja_id) VALUES ({user_id},{platja_id})
-                    ON DUPLICATE KEY UPDATE platja_id={platja_id};
-           """.format(user_id=user_id,platja_id=platja_id)
-        db = MySQLdb.connect(host="localhost",
+        db = MySQLdb.connect(host=scubabotdb_host,
                              user=scubabotdb_user,
                              passwd=scubabotdb_password,
                              db=scubabotdb_db)
         cur = db.cursor()
 
-        #print(db_query)
         cur.execute(db_query)
 
         db.commit()
@@ -34,3 +32,10 @@ def setUserPlatja(user_id,platja_id):
         return True
     except:
         return False
+
+def setUserPlatja(user_id,platja_id):
+    db_query = """
+            INSERT INTO bussejadors (telegram_id, platja_id) VALUES ({user_id},{platja_id})
+                ON DUPLICATE KEY UPDATE platja_id={platja_id};
+       """.format(user_id=user_id,platja_id=platja_id)
+    return runDBquery(db_query)
